e3f1ccae961a99ee2cc099021c5ad813bc122559
Carlos <cadriansalazarg@gmail.com>
2017-06-27 19:58:14 -0600

    I added the copyright form of some files

--- Vivado_ZedBoard/arm_sw/sw_test/lib_xmmult_hw.h
+++ Vivado_ZedBoard/arm_sw/sw_test/lib_xmmult_hw.h
@@ -1,3 +1,32 @@
+/*
+Note: This license has also been called the "New BSD License" or "Modified BSD License". See also the 2-clause BSD License.
+
+Copyright 2017 Erasmus Brain Project
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+*/
+
+/*
+#/******************************************************************************
+#* Vivado_ZedBoard/hls/lib_xmmult_hw.h
+#*
+#* Example taken of: https://www.xilinx.com/support/documentation/application_notes/xapp1170-zynq-hls.pdf
+#* Modified by : Carlos Salazar-García, 2017
+#* This source code is the kernel of the algorithm
+#* 
+#*
+#******************************************************************************/
+
 #ifndef H_LIB_EXAMPLE_HW_H
 #define H_LIB_EXAMPLE_HW_H
 
@@ -22,3 +51,14 @@ int Start_HW_Accelerator(void);
 int hls_init(XHls_accel *hls_Ptr);
 
 #endif
+
+/*
+#/******************************************************************************
+#* Vivado_ZedBoard/hls/lib_xmmult_hw.h
+#*
+#* Example taken of: https://www.xilinx.com/support/documentation/application_notes/xapp1170-zynq-hls.pdf
+#* Modified by : Carlos Salazar-García, 2017
+#* This source code is the kernel of the algorithm
+#* 
+#*
+#******************************************************************************/
