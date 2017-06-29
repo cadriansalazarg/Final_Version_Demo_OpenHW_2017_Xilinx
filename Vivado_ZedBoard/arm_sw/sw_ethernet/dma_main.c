0c19064879dcec0f899345c4fcd5c0b4f9f1589a
manchii <kaleb.23415@gmail.com>
2017-06-28 19:42:40 -0600

    deleted deprecated files

--- Vivado_ZedBoard/arm_sw/sw_ethernet/dma_main.c
+++ Vivado_ZedBoard/arm_sw/sw_ethernet/dma_main.c
@@ -1,17 +1,31 @@
 /*
-
 Note: This license has also been called the "New BSD License" or "Modified BSD License". See also the 2-clause BSD License.
 
-Copyright <YEAR> <COPYRIGHT HOLDER>
+Copyright 2017 Erasmus Brain Project
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
 */
 
+/*
+#******************************************************************************
+#* Vivado_ZedBoard/hls/main.cpp
+#*
+#* Example taken of: https://www.xilinx.com/support/documentation/application_notes/xapp1170-zynq-hls.pdf
+#* Modified by : Carlos Salazar-García, 2017
+#* This source code is the kernel of the algorithm
+#* 
+#*
+#******************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,8 +33,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 #include "xparameters.h"
 #include "xtmrctr.h"
 #include "xaxidma.h"
-#include "lib_xmmult_hw.h"
-//#include "xhls_accel.h"
+#include "lib_xaccel_hw.h"
 
 
 
@@ -37,7 +50,7 @@ XTmrCtr timer_dev;
 XAxiDma AxiDma;
 
 
-int init_dmakek(){
+int init_dma(){
 	XAxiDma_Config *CfgPtr;
 	int status;
 
@@ -79,11 +92,17 @@ int dma_main(int argc, char **argv)
 	int status, status1;
 
 
-	float data;
-	float iAppin[N_SIZE_TEST+NUM_NEIGH_CELLS];
-	float cellOut[N_SIZE_TEST+NUM_NEIGH_CELLS];
+	float data, iApp;
+	float iAppin[IO_N_SIZE];
+	float cellOut[TIME_MUX_FACTOR+TIME_MUX_FACTOR];
 
-	//unsigned int dma_size = (N_SIZE_TEST +NUM_NEIG_CELLS)* sizeof(float); //1024*4 = 4096 bbytes
+	unsigned int dma_size;
+
+	if (IO_N_SIZE>(TIME_MUX_FACTOR+TIME_MUX_FACTOR))
+		dma_size= (IO_N_SIZE)* sizeof(float); //1024*4 = 4096 bbytes
+	else
+		dma_size= (TIME_MUX_FACTOR+TIME_MUX_FACTOR)* sizeof(float); //1024*4 = 4096 bbytes
+
 
 	unsigned int init_time, curr_time, calibration;
 	unsigned int begin_time;
@@ -91,7 +110,7 @@ int dma_main(int argc, char **argv)
 	//unsigned int run_time_sw = 0;
 	unsigned int run_time_hw = 0;
 
-	//init_platform();
+	init_platform();
 
 	status1 = hls_init(&computenetwork);
 
@@ -107,7 +126,7 @@ int dma_main(int argc, char **argv)
 
 	/* ******************************************************************************* */
 	// Init DMA
-	status = init_dmakek();
+	status = init_dma();
 	if(status != XST_SUCCESS){
 		print("\rError: DMA init failed\n");
 		return XST_FAILURE;
@@ -141,7 +160,7 @@ int dma_main(int argc, char **argv)
 	
 	/** Start of Initiation */
 	
-	for(i=0;i<N_SIZE_TEST; i++){
+	for(i=0;i<TIME_MUX_FACTOR; i++){
 
     	data = -60;
     	XHls_accel_Write_local_state0_dend_V_dend_Words(&computenetwork, i,(int *) &data, 1);
@@ -181,16 +200,17 @@ int dma_main(int argc, char **argv)
 		XHls_accel_Write_local_state0_axon_Sodium_h_a_Words(&computenetwork, i,(int *) &data, 1);
 		data = 0.2369847;
 		XHls_accel_Write_local_state0_axon_Potassium_x_a_Words(&computenetwork, i,(int *) &data, 1);
+		data = 0.04;
+		XHls_accel_Write_Connectivity_Matrix_Words(&computenetwork, i,(int *) &data, 1);
     }
     
-    for(i=0;i<N_SIZE_TEST+NUM_NEIGH_CELLS; i++){
+    for(i=0;i<IO_N_SIZE; i++){
 		data = 0.04;
 		XHls_accel_Write_Connectivity_Matrix_Words(&computenetwork, i,(int *) &data, 1);
 	}
 
-	XHls_accel_Set_Mux_Factor(&computenetwork,N_SIZE_TEST);
-	XHls_accel_Set_N_Size(&computenetwork, N_SIZE_TEST);
-	//XHls_accel_Set_Conn_Matrix_Size(&computenetwork, N_SIZE_TEST);
+	XHls_accel_Set_Mux_Factor(&computenetwork,TIME_MUX_FACTOR);
+	XHls_accel_Set_N_Size(&computenetwork, IO_N_SIZE);
 	
 	/** End of Initiation */
 	
@@ -198,15 +218,23 @@ int dma_main(int argc, char **argv)
 	
 	for(i=0;i<SIM_STEP_TEST;i++){
 		
-		for (j=0; j<N_SIZE_TEST; j++){
- 		    iAppin[j] = 0.0f;
- 	    }
+
+		if(i>20000-1 && i<20500-1){ iApp = 6;} // start @ 1 because skipping initial values
+		else{ iApp = 0;}
+
+		for(j=0;j<TIME_MUX_FACTOR;j++){
+			iAppin[j] = iApp;
+		}
+
  	    
- 	    for (j=N_SIZE_TEST; j<N_SIZE_TEST+NUM_NEIGH_CELLS; j++){
+ 	    for (j=TIME_MUX_FACTOR; j<IO_N_SIZE; j++){
  		    if (i==0)	
 				iAppin[j] = -60.0f;
 			else
-				iAppin[j] = cellOut[j];
+				if (IO_N_SIZE>(TIME_MUX_FACTOR+TIME_MUX_FACTOR))
+					iAppin[j] = cellOut[TIME_MUX_FACTOR];
+				else
+					iAppin[j] = cellOut[j];
  	    }
 
 	
@@ -216,26 +244,40 @@ int dma_main(int argc, char **argv)
 		XTmrCtr_Reset(&timer_dev, XPAR_AXI_TIMER_DEVICE_ID);
 		begin_time = XTmrCtr_GetValue(&timer_dev, XPAR_AXI_TIMER_DEVICE_ID);
 		// Setup the HW Accelerator
-		status = Setup_HW_Accelerator(iAppin, cellOut, DMA_SIZE);
+		status = Setup_HW_Accelerator(iAppin, cellOut, dma_size);
 	
 
 		status = Start_HW_Accelerator();
-		status = Run_HW_Accelerator(iAppin, cellOut, DMA_SIZE);
+		status = Run_HW_Accelerator(iAppin, cellOut, dma_size);
 
 	
 		end_time = XTmrCtr_GetValue(&timer_dev, XPAR_AXI_TIMER_DEVICE_ID);
 		run_time_hw = end_time - begin_time - calibration;
-		xil_printf("Step %d:\n", i);
+		xil_printf("Step %d:\n", i+1);
 		xil_printf("Total run time for AXI DMA + HW accelerator is %d cycles\n", run_time_hw);
 
 		/* ******************************************************************************* */
 		
 
-		for(j=0; j<N_SIZE_TEST-1+NUM_NEIGH_CELLS; j++){
-    		printf("Va %d: %f\t", j+1, cellOut[j]);
-    	}
-    	
-    	printf("Va %d: %f\n", N_SIZE_TEST+NUM_NEIGH_CELLS, cellOut[N_SIZE_TEST-1+NUM_NEIGH_CELLS]);
+
+
+
+
+		#ifdef PRINTFAST
+			printf("Va  %d: %f,\t Va  %d: %f,\t Va  %d: %f,\t Va  %d: %f,\t", 0, cellOut[3], 1, cellOut[4], TIME_MUX_FACTOR-2, cellOut[TIME_MUX_FACTOR-2],TIME_MUX_FACTOR-1, cellOut[TIME_MUX_FACTOR-1]);
+			printf("Vd  %d: %f,\t Vd  %d: %f,\t Vd  %d: %f,\t Vd  %d: %f\n", 0, cellOut[TIME_MUX_FACTOR+2], 1, cellOut[TIME_MUX_FACTOR+2+1], TIME_MUX_FACTOR-2, cellOut[TIME_MUX_FACTOR+TIME_MUX_FACTOR-2],TIME_MUX_FACTOR-1, cellOut[TIME_MUX_FACTOR+TIME_MUX_FACTOR-1]);
+		#else
+
+		    for(j=0; j<IO_N_SIZE; j++){
+		    	if(j<TIME_MUX_FACTOR)
+		    		printf("Va %d: %f\t", j+1, cellOut[j]);
+		    	else
+		    		printf("Vd %d: %f\t", j+1-TIME_MUX_FACTOR, cellOut[j]);
+		    }
+
+		    printf("Vd %d: %f\n", IO_N_SIZE, cellOut[IO_N_SIZE-1]);
+
+		#endif
     	
 	}
 
@@ -243,3 +285,14 @@ int dma_main(int argc, char **argv)
     //cleanup_platform();
     return 0;
 }
+
+/*
+#******************************************************************************
+#* Vivado_ZedBoard/hls/main.cpp
+#*
+#* Example taken of: https://www.xilinx.com/support/documentation/application_notes/xapp1170-zynq-hls.pdf
+#* Modified by : Carlos Salazar-García, 2017
+#* This source code is the kernel of the algorithm
+#* 
+#*
+#******************************************************************************/
